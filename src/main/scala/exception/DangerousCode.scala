ebd95c6b17cd05479730f800180bfb09fc875f81
tammosminia <tammosminia@gmail.com>
2022-10-11 17:21:18 +0200

    zio env

--- src/main/scala/exception/DangerousCode.scala
+++ src/main/scala/exception/DangerousCode.scala
@@ -8,12 +8,8 @@ object DangerousCode {
     case 1 => ZIO.fail("explicit failure")
     case 2 => ZIO.attempt(throw new RuntimeException("failure from a caught exception"))
     case 3 => ZIO.die(new RuntimeException("explicit defect"))
-    case 4 => uncaught
+    case 4 => ZIO.unit.map(_ => throw new RuntimeException("uncaught exception"))
     case 5 => ZIO.fail(new RuntimeException("first exception")).ensuring(ZIO.die(new RuntimeException("second exception")))
     case 6 => ZIO.attempt(throw new StackOverflowError("Fatal error that cannot be caught."))
   }
-
-  def uncaught: ZIO[Any, Nothing, Unit] = for {
-    u <- ZIO.unit
-  } yield throw new RuntimeException("uncaught exception")
 }
