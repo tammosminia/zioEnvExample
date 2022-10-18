f42d5b247ea4c2527908a2457b07a229ca6ecc30
tammosminia <tammosminia@gmail.com>
2022-10-18 17:01:58 +0200

    zio env

--- src/main/scala/runtime/UpdateRuntimeAppWithUserId.scala
+++ src/main/scala/runtime/UpdateRuntimeAppWithUserId.scala
@@ -5,21 +5,24 @@ import zio.{RIO, Runtime, Scope, Unsafe, ZEnvironment, ZIO, ZLayer, durationInt}
 object UpdateRuntimeAppWithUserId extends App {
   Unsafe.unsafe { implicit unsafe =>
     Console.println("Example app start")
-    val runtime: Runtime.Scoped[AppConfig with Database] = zio.Runtime.unsafe.fromLayer(AppConfig.layer ++ (AppConfig.layer >>> Database.layer))
+    val runtime: Runtime.Scoped[Database] = zio.Runtime.unsafe.fromLayer(AppConfig.layer >>> Database.layer)
     case class UserId(id: String)
+    case class TrackingId(id: Int)
     val userId = UserId("123")
     val updatedRuntime = runtime.mapEnvironment(_.add(userId))
-    run()
-    run()
+    run(TrackingId(1))
+    run(TrackingId(2))
     updatedRuntime.unsafe.shutdown()
     Console.println("Example app end")
 
-    def domainAction: RIO[Database with UserId, Unit] = ZIO.serviceWithZIO[UserId] { u =>
-      Database.write(s"new domain object for user ${u.id}")
-    }
+    def domainAction: RIO[Database with UserId with TrackingId, Unit] = for {
+      u <- ZIO.service[UserId]
+      t <- ZIO.service[TrackingId]
+      r <- Database.write(s"new domain object for user ${u.id}, trackingId: ${t.id}")
+    } yield r
 
-    def run(): Unit = {
-      updatedRuntime.unsafe.run(domainAction).getOrThrowFiberFailure()
+    def run(t: TrackingId): Unit = {
+      updatedRuntime.unsafe.run(domainAction.provideSome[Database with UserId](ZLayer.succeed(t))).getOrThrowFiberFailure()
     }
   }
 }
